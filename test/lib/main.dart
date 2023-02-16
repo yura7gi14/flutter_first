1109f45ec99c22ad968b587db00f5057abfa1fde
yura7gi14 <yura7gi14@github.com>
2023-02-17 04:08:25 +0900

    メモアプリ現状

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -1,134 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:logger/logger.dart';
+import 'package:project/screens/edit.dart';
+import 'package:project/db/databasetest.dart';
 
-void main() {
-  runApp(MemoApp());
-}
+final logger = Logger();
 
-class MemoApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Memo App',
-      home: MemoList(),
-    );
-  }
-}
+void main() => runApp(const MyApp());
 
-class MemoList extends StatefulWidget {
-  @override
-  _MemoListState createState() => _MemoListState();
-}
-
-class _MemoListState extends State<MemoList> {
-  List<String> memos = [];
-
-  TextEditingController controller = TextEditingController();
-
-  void addMemo(String memo) {
-    setState(() {
-      memos.add(memo);
-    });
-  }
-
-  void editMemo(int index, String memo) {
-    setState(() {
-      memos[index] = memo;
-    });
-  }
-
-  void deleteMemo(int index) {
-    setState(() {
-      memos.removeAt(index);
-    });
-  }
-
-  void openAddMemoDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Add Memo'),
-          content: TextField(
-            controller: controller,
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Add'),
-              onPressed: () {
-                addMemo(controller.text);
-                controller.clear();
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
+/// The route configuration.
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const EditPage();
       },
-    );
-  }
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'register',
+          builder: (BuildContext context, GoRouterState state) {
+            return const DataBaseTestPage();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
-  void openEditMemoDialog(int index, String memo) {
-    TextEditingController editController = TextEditingController(text: memo);
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Edit Memo'),
-          content: TextField(
-            controller: editController,
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Save'),
-              onPressed: () {
-                editMemo(index, editController.text);
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
+/// The main app.
+class MyApp extends StatelessWidget {
+  /// Constructs a [MyApp]
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Memo List'),
-      ),
-      body: ListView.builder(
-        itemCount: memos.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: InkWell(
-              onTap: () {
-                openEditMemoDialog(index, memos[index]);
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(memos[index]),
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: openAddMemoDialog,
-        child: Icon(Icons.add),
-      ),
+    return MaterialApp.router(
+      routerConfig: _router,
     );
   }
 }
